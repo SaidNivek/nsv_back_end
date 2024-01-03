333b5455ffc724a906bbfc9ce50578611b97b580
SaidNivek <kevin.vi.dias@gmail.com>
2024-01-03 16:02:27 -0500

    Add Hours schema

---
+++ schemas/hours.js
@@ -0,0 +1,17 @@
+export default {
+    name:'hours',
+    title:'Hours',
+    type:'document',
+    fields: [
+        {
+            name:'day',
+            title:'Day',
+            type: 'string'
+        },
+        {
+            name:'hours',
+            title:'Hours',
+            type: 'string'
+        },
+    ]
+}
--- schemas/index.js
+++ schemas/index.js
@@ -1,1 +1,2 @@
-export const schemaTypes = []
+import hours from "./hours"
+export const schemaTypes = [hours]
