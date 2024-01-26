97a19c8332fca6981da5c147290201ee4f976672
SaidNivek <kevin.vi.dias@gmail.com>
2024-01-26 17:34:14 -0500

    Create reviews schema

--- schemas/index.js
+++ schemas/index.js
@@ -2,5 +2,6 @@ import locations from "./locations"
 import promotions from "./promotions"
 import values from "./values"
 import brands from "./brands"
+import reviews from "./reviews"
 
-export const schemaTypes = [locations, promotions, values, brands]
+export const schemaTypes = [locations, promotions, values, brands, reviews]
