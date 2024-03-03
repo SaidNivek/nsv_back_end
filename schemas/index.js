0404958a2e73534b99331ce58711dc6194542429
SaidNivek <kevin.vi.dias@gmail.com>
2024-03-03 12:00:30 -0500

    Add contact schema for messages from the system

--- schemas/index.js
+++ schemas/index.js
@@ -3,5 +3,6 @@ import promotions from "./promotions"
 import values from "./values"
 import brands from "./brands"
 import reviews from "./reviews"
+import contact from "./contact"
 
-export const schemaTypes = [locations, promotions, values, brands, reviews]
+export const schemaTypes = [locations, promotions, values, brands, reviews, contact]
